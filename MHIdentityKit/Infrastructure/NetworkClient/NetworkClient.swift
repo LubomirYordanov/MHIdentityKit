959830084232265270d24c4154201fa1b4ea2783
Lyubomir Yordanov <lyubomir.yordanov@eldersoss.com>
2025-08-25 17:46:50 +0300

    Make network client completion handler sendable and create async function for the perform.

--- MHIdentityKit/Infrastructure/NetworkClient/NetworkClient.swift
+++ MHIdentityKit/Infrastructure/NetworkClient/NetworkClient.swift
@@ -12,6 +12,20 @@ import Foundation
 public protocol NetworkClient {
     
     ///Performs a request and execute a completion handler when it is done
-    func perform(_ request: URLRequest, completion: @escaping (NetworkResponse) -> Void)
+    func perform(_ request: URLRequest, completion: @escaping @Sendable (NetworkResponse) -> Void)
 }
 
+extension NetworkClient {
+    
+    @available(iOS 13, tvOS 13, macOS 10.15, watchOS 6, *)
+    public func perform(_ request: URLRequest) async -> NetworkResponse {
+        
+        return await withCheckedContinuation { continuation in
+            
+            self.perform(request) { response in
+                
+                continuation.resume(returning: response)
+            }
+        }
+    }
+}
