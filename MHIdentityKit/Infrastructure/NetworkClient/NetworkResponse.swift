430001bd115669d2d645486bd11be106307fd605
Lyubomir Yordanov <lyubomir.yordanov@eldersoss.com>
2025-08-26 10:39:08 +0300

    Made network response unchecked sendable

--- MHIdentityKit/Infrastructure/NetworkClient/NetworkResponse.swift
+++ MHIdentityKit/Infrastructure/NetworkClient/NetworkResponse.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public struct NetworkResponse {
+public struct NetworkResponse : @unchecked Sendable {
     
     public var data: Data?
     public var response: URLResponse?
